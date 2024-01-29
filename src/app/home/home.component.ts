692d4729f44858891c0c3831a931be376f1d7e83
Marcos8060 <marcosgav80@gmail.com>
2024-01-29 00:26:40 -0500

    made a minor change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { HousingLocationInterface } from './../housing-location';
-import { Component,Inject, inject } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { CommonModule } from '@angular/common';
 import { HousingService } from '../housing.service';
